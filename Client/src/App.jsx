3f2ff4c5113d776918918ba7df85e3da166779a0
praveensunar <140327889+praveensunar@users.noreply.github.com>
2025-10-06 14:20:37 +0530

    file name

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Dashboard from './components/Dashboard';   // ✅ use full Dashboard, not QRScanner
+import Dashboard from './components/DashBoard';   // ✅ use full Dashboard, not QRScanner
 import Login from './page/login';
 
 // API base
